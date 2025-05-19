d8a92e7cf445c06ed716b3102812afd9471cc17f
LautiSarachaga <lautarosarachaga@hotmail.com>
2025-05-18 22:44:28 -0300

    arreglo Mp

--- tienda-backend/index.js
+++ tienda-backend/index.js
@@ -14,9 +14,8 @@ const MONGODB_URI = process.env.MONGODB_URI;
 const MP_TOKEN = process.env.MP_TOKEN;
 
 // Configurar MercadoPago con el token de entorno
-mercadopago.configure({
-  access_token: MP_TOKEN
-});
+mercadopago.configurations.setAccessToken('MP_TOKEN');
+
 
 // Conexión a MongoDB
 mongoose.connect(MONGODB_URI, {
